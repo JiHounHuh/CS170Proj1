1dfbce3fe9509e77c2c87e5072b3a76485ee7fc8
PatrickWumbo <jhuh009@ucr.edu>
2019-11-06 00:44:14 -0800

    Finished implmentations

--- proj1.cpp
+++ proj1.cpp
@@ -8,73 +8,46 @@
 using namespace std;
 
 
-
-
-int general() {
- 
-}
-
-
-
- 
-
-int uniform(int alpha, int beta) {
-    int row, col;
-    row = 2;
-    col = 2;
-
-    // Find head node
-    // left
-    // down
-    // right
-    uniform(1,2); // recursion through the nodes to find best path
-
-}
-
-int AStarMisplaced() {
-    
-}
-
-int AStarManhattan() {
-
-}
-
-
-
-// int left(node->head) {
-//     if(node->left != NULL)
-//         curr->left;
-//     uniform(curr,goal);
-
-    
-// }
-
-
-
-
 int main() {
     int check, type;
+    node*B = new node();
     cout << "Type '1' to use a default puzzle, or '2' to enter your own puzzle." << endl;
     cin >> check;
+
+    switch (check)
+    {
+    case 1:
+        /* code */
+        // USING DEFAULT
+        break;
+    case 2:
+        // MAKE INPUT FOR CUSTOM PUZZLE
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
     cout << "Type '1' for uniform, '2' for Misplaced, '3' for Manhattan." << endl;
     cin >> type;
     switch (type)
     {
     case 1:
         /* code */
-
+        cheese = new ucs();
         break;
     case 2:
         /* code */
-        
+        cheese = new misplaced();
         break;
     case 3:
         /* code */
-        
+        cheese = new manhattan();
         break;
     
     default:
+        cheese = new ucs();
         break;
     }
-
+    generalSearch(B);
 }
--- proj1.h
+++ proj1.h
@@ -4,14 +4,28 @@
 #include <algorithm>
 #include <stdlib.h>
 #include <iostream>
+
 using namespace std;
 
+vector<vector<int>> goal = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
 
+class node{
+    public:
+        int g,h;
+        vector<vector<int>> state;
+        node(){state = {{4, 1, 2},{8, 3, 6},{5, 7, 0}}; g = 0; h = 0;}
+        node(node*copy){
+            this->g = copy->g;
+            this->h = copy->h;
+            this->state = copy->state;
+        }
 
+};
 class searches{
     public:
         virtual void heuristic(node*) = 0;
 };
+
 
 class ucs:public searches{
     public:
@@ -20,27 +34,40 @@ class ucs:public searches{
 
 class misplaced:public searches{
     public:
-        void heuristic(node* h){}
+        void heuristic(node* h){
+            int value = 0;
+            for(int i = 0; i < 3; i++) {
+                for(int j = 0; j < 3; j++) {
+                    if(h->state[i][j] != goal[i][j]) value++;
+                }
+            }
+            h->h = value;
+        }
 };
 
 class manhattan:public searches{
     public:
-        void heuristic(node* h){}
-};
-
-vector<vector<int>> goal = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '0'}};
-class node{
-    public:
-        int g,h;
-        vector<vector<int>> state;
-        node(){state = {{'4', '1', '2'},{'8', '3', '6'},{'5', '7', '0'}}; g = 0; h = 0;}
-        node(node*copy){
-            this->g = copy->g;
-            this->h = copy->h;
-            this->state = copy->state;
+        void heuristic(node* h){
+            int temp1 = 0,temp2 = 0,temp3 = 0,total = 0;
+            for(int i = 0; i < 3; i++) {
+                for(int j = 0; j < 3; j++) {
+                    temp1 = h->state[i][j];
+                    if(temp1 != 0) {
+                        temp2 = (temp1 - 1)/3;
+                        temp3 = (temp1 - 1)%3;
+                        total += abs(temp2 - i) + abs(temp3 - j);
+                    }
+                    
+                    
+                }
+            }
+            h->h = total;
         }
-
 };
+
+searches* cheese = new ucs();
+
+
 
 bool moveUp(node *B) {
     for(int i = 1; i < 3; i++) {
@@ -94,7 +121,30 @@ bool moveRight(node *B) {
     return false;
 }
 
+void printBoard(node *A) {
+    for(int i = 0; i < 3; i++) {
+        cout << "\t\t";
+        for(int j = 0; j < 3; j++) {
+            cout << A->state[i][j] << " ";
+        }
+        if(i != 2) cout << endl;
+    }
+}
+
+void printFirst(node *A) {
+    for(int i = 0; i < 3; i++) {
+        if(i != 0) cout << "\t\t";
+        for(int j = 0; j < 3; j++) {
+            cout << A->state[i][j] << " ";
+        }
+        if(i != 2) cout << endl;
+    }
+}
+
+
 void generalSearch(node *A) {
+    unsigned int expanded = 0;
+    unsigned int maxnum = 0;
     auto comp = [](const node*A, node*B){return ((A->g + A->h) > (B->g + B->h)); };
     priority_queue < node*,vector < node* >, decltype(comp)> pqueue(comp);
     node *curr;
@@ -102,17 +152,33 @@ void generalSearch(node *A) {
     pqueue.push(A);
     while(!pqueue.empty()) {
         curr = pqueue.top();
+        if(pqueue.size() > maxnum) maxnum = pqueue.size();
         pqueue.pop();
         if(curr->state == goal) {
-            cout << "SUCCESS" << endl;
+            cout << "Goal!!\n" << endl;
+            cout << "To solve this problem the search algorithm expanded a total of " << expanded << " nodes." << endl;
+            cout << "The maximum number of nodes in the queue at any one time was " << maxnum << '.' << endl;
+            cout << "The depth of the goal node was " << curr->g << '.' << endl;
             return;
         }
+        
+        if(pqueue.empty()){
+            cout << "Expanding state ";
+            printFirst(curr);
+            cout << endl << endl;
+        }
+        else{
+            cout << "The best state to expand with a g(n)= " << curr->g << " and h(n) = " << curr->h << " is..." << endl;
+            printBoard(curr); 
+            cout << "Expanding this node..." << endl << endl;
+        }
         curr->g++;
+        
         temp = new node(curr);
-        if(moveUp(temp)) pqueue.push(temp); temp = new node(curr);
-        if(moveDown(temp)) pqueue.push(temp); temp = new node(curr);
-        if(moveLeft(temp)) pqueue.push(temp); temp = new node(curr);
-        if(moveRight(temp)) pqueue.push(temp);
+        if(moveUp(temp)){ cheese->heuristic(temp);expanded++; /*set heuristic value to node*/ pqueue.push(temp); }  temp = new node(curr);
+        if(moveDown(temp)){ cheese->heuristic(temp);expanded++; pqueue.push(temp); } temp = new node(curr);
+        if(moveLeft(temp)){ cheese->heuristic(temp);expanded++; pqueue.push(temp); } temp = new node(curr);
+        if(moveRight(temp)){ cheese->heuristic(temp);expanded++; pqueue.push(temp); }
     }
     return;
 }
