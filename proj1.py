5ee6515a85b3381e7fd6e768c2e4af32ebaf4138
PatrickWumbo <jhuh009@ucr.edu>
2019-11-05 20:32:09 -0800

    Random testing

--- proj1.py
+++ proj1.py
@@ -1,5 +1,6 @@
 import sys
 import copy
+from copy import deepcopy
 import queue
 import math
 
@@ -12,21 +13,22 @@ MAXSIZE = (math.sqrt(PUZZLETYPE + 1))
   #  def __init__(self):
    #     self.elements = []
 
-#Class Puzzle
-# def zero_position
-#   @cells.index(0)
-# end
-# def swap swap_index
-#   new_cells = @cells.clone
-#   new_cells[zero_position] = new_cells[swap_index]
-#   new_cells[swap_index] = 0
-#   Puzzle.new new_cells
-# end
-#end
-
+class Node():
+    def __init__(self, head=None, pos=None):
+        self.head = head
+        self.pos = pos
+        
+        self.f = 0
+        self.g = 0
+        self.h = 0
+    def __eq__(self, other): # Return if they are equal
+        return self.pos == other.pos
+        
 class Movement:
     alpha = 0
     beta = 0
+    row = 2
+    col = 2
     def __init__ (self, alpha = 0, beta = 0):
         self.alpha = alpha
         self.beta = beta
@@ -40,6 +42,16 @@ class Movement:
     # Move Down
     # Move Left
     # Move Right
+    def moveUp(self):
+        newSpot = list(self.state)
+        if(row < 2):
+            newSpot[alpha][beta], newSpot[alpha-1][beta] = newSpot[alpha-1][beta], newSpot[alpha][beta]
+            self._blank.set(x = (alpha - 1))
+            
+        
+
+
+
    # def branch_toward(direction):
         #blank_position = Puzzle.zero_position
        # blankx = blank_position % 3
@@ -55,6 +67,17 @@ class Movement:
         #end
         #State.new Puzzle.swap(cell), @path + [direction] if cell
     #end
+openList = []
+closedList = []
+
+def copy(self, rootNode): # create similar matrix copy
+    temp = []
+    for i in rootNode:
+        init = []
+        for j in i:
+            init.append(j)
+        temp.append(init)
+    return temp
 
 #class Puzzle:
 #    def __init__(self, init = INITIALSTATE, shape = DIMENSION):
@@ -72,8 +95,8 @@ goal_state = str(GOALSTATE)
 print("Hashed init state is: " + str(hash(init_state)))
 print("Hashed goal state is: " + str(hash(goal_state)))
 #Record cost and estimated distance
-row = 2
-col = 2
+row = 2 
+col = 2 
 #if(i < col):
     #can move right
     
@@ -87,22 +110,31 @@ col = 2
     #can move up
 
 
+def general_search(problem, heuristic):
+    #nodes = Make_Queue(Make_Node(problem.INITIALSTATE))
+    print("test")
+
+def uniformCostSearchHeurisitic(problem):
+    return 0 # A-Star with heuristic as 0
+
+def misplacedTileSearchHeuristic(problem):
+    print("test")    
 
 
 
-
-
+print("Initial State", end=' ')
 for i in range(3):
     print('')
     for j in range(3):
         print(INITIALSTATE[i][j], end =' ')
 
 print()
-print(INITIALSTATE[2][2])
+
 temp_state = INITIALSTATE[1][2]
 INITIALSTATE[1][2] = INITIALSTATE[2][2]
 INITIALSTATE[2][2] = temp_state
-
+print()
+print("Goal State",end=' ')
 for i in range(3):
     print('')
     for j in range(3):
